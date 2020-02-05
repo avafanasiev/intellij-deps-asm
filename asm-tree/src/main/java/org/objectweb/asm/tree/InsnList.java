c565dad0817608108f51505b1c1686deccda2120
Alexey Afanasiev <megabochka@gmail.com>
2020-02-06 00:03:31 +0300

    Update InsnList.java

--- asm-tree/src/main/java/org/objectweb/asm/tree/InsnList.java
+++ asm-tree/src/main/java/org/objectweb/asm/tree/InsnList.java
@@ -4,7 +4,7 @@
 // are met:asd
 // 1. Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
+// 2. Redistributions in binary form must rep roduce the above copyright
 //    notice, this list of conditions and the following disclaimer in the
 //    documentation and/or other materials provided with the distribution.
 // 3. Neither the name of the copyright holders nor the names of its
